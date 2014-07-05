19723c3eb489f0d81cd427420179233399f8e31c
Parlin.Yu <bupt.aswmtjdsj@gmail.com>
2014-07-05 11:48:04 +0800

    CPP related

---
+++ cpp/common.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
+#endif
